5e9d7cf5e117834b897ef6881a7cba2e4ba03603
YAS <flod@yasbox.com>
2022-09-07 18:11:40 +0900

    登録モーダル内レイアウト変更

--- src/components/Modules/MyButton.js
+++ src/components/Modules/MyButton.js
@@ -3,7 +3,7 @@ import { overrideTailwindClasses } from 'tailwind-override'
 const MyButton = ({ type = 'button', className, ...props }) => (
     <button
         type={type}
-        className={overrideTailwindClasses(`inline-flex items-center px-3 py-1 bg-sky-500 border border-transparent rounded-md font-semibold text-sm sm:text-lg text-white uppercase tracking-widest hover:bg-sky-600 active:bg-sky-400 focus:outline-none focus:border-sky-600 focus:ring ring-sky-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
+        className={overrideTailwindClasses(`inline-flex items-center m-2 px-3 py-1 bg-sky-500 border border-transparent rounded-full font-semibold text-sm sm:text-md text-white uppercase tracking-widest hover:bg-sky-600 active:bg-sky-400 focus:outline-none focus:border-sky-600 focus:ring ring-sky-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
         {...props}
     />
 )
@@ -11,7 +11,15 @@ const MyButton = ({ type = 'button', className, ...props }) => (
 export const MyButton_lg = ({ type = 'button', className, ...props }) => (
     <button
         type={type}
-        className={overrideTailwindClasses(`inline-flex items-center px-4 py-2 bg-sky-500 border border-transparent rounded-md font-semibold text-xl text-white uppercase tracking-widest hover:bg-sky-600 active:bg-sky-400 focus:outline-none focus:border-sky-600 focus:ring ring-sky-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
+        className={overrideTailwindClasses(`inline-flex items-center m-2 px-8 py-2 bg-gray-200 border border-transparent rounded-full font-semibold text-md sm:text-lg text-black uppercase tracking-widest hover:bg-gray-300 active:bg-gray-300 focus:outline-none focus:border-gray-400 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
+        {...props}
+    />
+)
+
+export const MyButton_md = ({ type = 'button', className, ...props }) => (
+    <button
+        type={type}
+        className={overrideTailwindClasses(`inline-flex items-center m-2 px-6 py-2 bg-gray-200 border border-transparent rounded-full font-semibold text-sm sm:text-md text-black uppercase tracking-widest hover:bg-gray-300 active:bg-gray-300 focus:outline-none focus:border-gray-400 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
         {...props}
     />
 )
@@ -19,15 +27,15 @@ export const MyButton_lg = ({ type = 'button', className, ...props }) => (
 export const MyButton_sm = ({ type = 'button', className, ...props }) => (
     <button
         type={type}
-        className={overrideTailwindClasses(`inline-flex items-center px-2 py-1 bg-sky-500 border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest hover:bg-sky-600 active:bg-sky-400 focus:outline-none focus:border-sky-600 focus:ring ring-sky-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
+        className={overrideTailwindClasses(`inline-flex items-center m-2 px-4 py-1 bg-gray-200 border border-transparent rounded-full font-semibold text-xs sm:text-sm text-black uppercase tracking-widest hover:bg-gray-300 active:bg-gray-300 focus:outline-none focus:border-gray-400 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
         {...props}
     />
 )
 
-export const MyButton_order = ({ type = 'button', className, ...props }) => (
+export const MyButton_add = ({ type = 'button', className, ...props }) => (
     <button
         type={type}
-        className={overrideTailwindClasses(`inline-flex items-center px-4 py-2 bg-yellow-400 border border-transparent rounded-md font-bold text-black uppercase tracking-widest hover:bg-yellow-300 active:bg-yellow-300 focus:outline-none focus:border-yellow-400 focus:ring ring-yellow-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
+        className={overrideTailwindClasses(`min-w-[50px] min-h-[50px] inline-flex items-center m-2 justify-center bg-gray-200 border border-transparent rounded-full font-bold text-black uppercase tracking-widest hover:bg-gray-300 active:bg-gray-300 focus:outline-none focus:border-gray-400 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
         {...props}
     />
 )
@@ -35,7 +43,7 @@ export const MyButton_order = ({ type = 'button', className, ...props }) => (
 export const MyButton_detail = ({ type = 'button', className, ...props }) => (
     <button
         type={type}
-        className={overrideTailwindClasses(`inline-flex items-center px-2 py-1 bg-gray-300 border border-transparent rounded-md font-semibold text-sm text-black uppercase tracking-widest hover:bg-gray-400 active:bg-gray-400 focus:outline-none focus:border-gray-300 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
+        className={overrideTailwindClasses(`inline-flex items-center m-2 px-6 py-4 bg-gray-200 border border-transparent rounded-full font-semibold text-sm text-black uppercase tracking-widest hover:bg-gray-300 active:bg-gray-300 focus:outline-none focus:border-gray-400 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ${className}`)}
         {...props}
     />
 )
